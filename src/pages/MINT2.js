2514635272e921ae4b6a37564bcbf808d2607756
b_oxygen <rhdtkqkr@gmail.com>
2023-01-24 14:28:14 +0900

    커스터마이징 부분 완성

--- src/pages/MINT2.js
+++ src/pages/MINT2.js
@@ -15,9 +15,9 @@ import "swiper/scss/navigation";
 
 const MINT2 = () => {
   const [hex, setHex] = useState("#fff");
-  const [backgroundColor, setBackgroundColor] = useState(
-    "https://gist.githubusercontent.com/B-oxygen/7125b95688d9e52eb329b5b74e62c00b/raw/9b45af604757456978020c3ad243dfce3b939260/backgroundColor.svg"
-  );
+  // const [backgroundColor, setBackgroundColor] = useState(
+  //   "https://gist.githubusercontent.com/B-oxygen/7125b95688d9e52eb329b5b74e62c00b/raw/9b45af604757456978020c3ad243dfce3b939260/backgroundColor.svg"
+  // );
   const [skinSort, setSkinSort] = useState("https://iili.io/HT56dbI.png");
   const [hairSort, setHairSort] = useState("https://iili.io/HT56Ksn.png");
   const [eyesSort, setEyesSort] = useState("https://iili.io/HT56oJ4.png");
@@ -25,7 +25,9 @@ const MINT2 = () => {
   const [clothesSort, setClothesSort] = useState("https://iili.io/HT56agj.png");
 
   const navigate = useNavigate();
-  const swiperRef = useRef(null);
+  const hairSwiperRef = useRef(null);
+  const eyeBrowsSwiperRef = useRef(null);
+  const eyesSwiperRef = useRef(null);
 
   const onLOGOClick = useCallback(() => {
     navigate("/");
@@ -67,6 +69,14 @@ const MINT2 = () => {
     <div className={styles.mint2}>
       <div className={styles.hair} id="HAIR">
         <div className={styles.hairChild} />
+
+        <div className={styles.hairText}>HAIR</div>
+        <img
+          className={styles.nametagw4Icon}
+          alt=""
+          src="../nametagw-7@1x.png"
+        />
+
         <div className={styles.hairItem}>
           <Swiper
             pagination={{
@@ -76,14 +86,14 @@ const MINT2 = () => {
             className={styles.mySwiper}
             slidesPerView={1}
             onBeforeInit={(swiper) => {
-              swiperRef.current = swiper;
+              hairSwiperRef.current = swiper;
             }}
           >
             <SwiperSlide>
               <img
                 className={styles.hair1}
                 alt=""
-                src="https://iili.io/HT56Ksn.png"
+                src="../hair_1@1x.png"
                 onClick={() => {
                   setHairSort("https://iili.io/HT56Ksn.png");
                 }}
@@ -93,7 +103,7 @@ const MINT2 = () => {
               <img
                 className={styles.hair2}
                 alt=""
-                src="https://iili.io/HT56B0G.png"
+                src="../hair_1@1x.png"
                 onClick={() => {
                   setHairSort("https://iili.io/HT56B0G.png");
                 }}
@@ -103,7 +113,7 @@ const MINT2 = () => {
               <img
                 className={styles.hair3}
                 alt=""
-                src="https://iili.io/HT56Cgf.png"
+                src="../hair_1@1x.png"
                 onClick={() => {
                   setHairSort("https://iili.io/HT56Cgf.png");
                 }}
@@ -113,97 +123,279 @@ const MINT2 = () => {
               <img
                 className={styles.hair4}
                 alt=""
-                src="https://iili.io/HT56x5l.png"
+                src="../hair_1@1x.png"
                 onClick={() => {
                   setHairSort("https://iili.io/HT56x5l.png");
                 }}
               />
             </SwiperSlide>
-            <SwiperSlide>
-              <img className={styles.hair5} alt="추가 될 예정" src="" />
-            </SwiperSlide>
           </Swiper>
         </div>
-        a
+
         <img
-          className={styles.nametagw4Icon}
-          alt=""
-          src="../nametagw-7@1x.png"
-        />
-        <div className={styles.hairText}>HAIR</div>
-        <img
-          className={styles.hairInner}
-          alt=""
-          src="../polygon-3.svg"
-          onClick={() => swiperRef.current?.slideNext()}
-        />
-        <img
-          className={styles.hairChild1}
+          className={styles.hairButtonPrev}
           alt=""
           src="../polygon-4.svg"
-          onClick={() => swiperRef.current?.slidePrev()}
+          onClick={() => hairSwiperRef.current?.slidePrev()}
         />
-        <img className={styles.hairChild2} alt="" src="../ellipse-4.svg" />
-        <img className={styles.hairChild3} alt="" src="../ellipse-5.svg" />
-        <img className={styles.hairChild4} alt="" src="../ellipse-6.svg" />
+        <img
+          className={styles.hairButtonNext}
+          alt=""
+          src="../polygon-3.svg"
+          onClick={() => hairSwiperRef.current?.slideNext()}
+        />
       </div>
+
       <div className={styles.clothes} id="Clothes">
         <div className={styles.clothesChild} />
-        <div className={styles.clothesItem} />
-        <div className={styles.clothes1}>CLOTHES</div>
-        <img className={styles.icon2} alt="" src="../clothes@2x.png" />
+
+        <div className={styles.clothesText}>CLOTHES</div>
         <img
           className={styles.nametagw6Icon}
           alt=""
           src="../nametagw-6@2x.png"
         />
-        <img className={styles.clothesInner} alt="" src="../ellipse-4.svg" />
-        <img className={styles.clothesChild1} alt="" src="../ellipse-5.svg" />
-        <img className={styles.clothesChild2} alt="" src="../ellipse-6.svg" />
+
+        <div className={styles.clothesItem}>
+          <Swiper
+            pagination={{
+              type: "progressbar",
+            }}
+            modules={[Pagination, Navigation]}
+            className={styles.mySwiper}
+            slidesPerView={1}
+            onBeforeInit={(swiper) => {
+              eyesSwiperRef.current = swiper;
+            }}
+          >
+            <SwiperSlide>
+              <img
+                className={styles.clothes1}
+                alt=""
+                src="../clothes@2x.png"
+                onClick={() => {
+                  setClothesSort("https://iili.io/HT56agj.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.clothes1}
+                alt=""
+                src="../clothes@2x.png"
+                onClick={() => {
+                  setClothesSort("https://iili.io/HT56ldx.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.clothes1}
+                alt=""
+                src="../clothes@2x.png"
+                onClick={() => {
+                  setClothesSort("https://iili.io/HT5605Q.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.clothes1}
+                alt=""
+                src="../clothes@2x.png"
+                onClick={() => {
+                  setClothesSort("https://iili.io/HT561eV.png");
+                }}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
+
       <div className={styles.eyebrows} id="Eyebrows">
         <div className={styles.eyesChild} />
+
+        <div className={styles.brows}>BROWS</div>
         <img
           className={styles.nametagw4Icon}
           alt=""
           src="../nametagw-7@1x.png"
         />
-        <div className={styles.brows}>BROWS</div>
+
+        <div className={styles.eyebrowsItem}>
+          <Swiper
+            pagination={{
+              type: "progressbar",
+            }}
+            modules={[Pagination, Navigation]}
+            className={styles.mySwiper}
+            slidesPerView={1}
+            onBeforeInit={(swiper) => {
+              eyeBrowsSwiperRef.current = swiper;
+            }}
+          >
+            <SwiperSlide>
+              <img
+                className={styles.eyebrows1}
+                alt=""
+                src="../Eyebrows_1@2x.png"
+                onClick={() => {
+                  setEyebrowSort("https://iili.io/HT56AX9.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.eyebrows2}
+                alt=""
+                src="../Eyebrows_1@2x.png"
+                onClick={() => {
+                  setEyebrowSort("https://iili.io/HT56Rse.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.eyebrows3}
+                alt=""
+                src="../Eyebrows_1@2x.png"
+                onClick={() => {
+                  setEyebrowSort("https://iili.io/HT56Y0b.png");
+                }}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+
         <img
-          className={styles.eyebrowsItem}
+          className={styles.eyebrowsInner}
           alt=""
-          src="../rectangle-2@1x.png"
+          src="../polygon-5.svg"
+          onClick={() => eyeBrowsSwiperRef.current?.slideNext()}
         />
-        <img className={styles.eyebrowsInner} alt="" src="../polygon-5.svg" />
-        <img className={styles.eyebrowsChild1} alt="" src="../polygon-6.svg" />
-        <img className={styles.icon1} alt="" src="../Eyebrows_1@2x.png" />
+        <img
+          className={styles.eyebrowsChild1}
+          alt=""
+          src="../polygon-6.svg"
+          onClick={() => eyeBrowsSwiperRef.current?.slidePrev()}
+        />
       </div>
+
       <div className={styles.eyes} id="Eyes">
         <div className={styles.eyesChild} />
+
+        <div className={styles.eyesText}>EYES</div>
         <img
           className={styles.nametagw5Icon}
           alt=""
           src="../nametagw-7@1x.png"
         />
-        <div className={styles.eyes1}>EYES</div>
-        <div className={styles.eyesItem} />
-        <img className={styles.eyesInner} alt="" src="../polygon-6.svg" />
-        <img className={styles.polygonIcon} alt="" src="../polygon-5.svg" />
-        <img className={styles.icon} alt="" src="../eyes_1@2x.png" />
+
+        <div className={styles.eyesItem}>
+          <Swiper
+            pagination={{
+              type: "progressbar",
+            }}
+            modules={[Pagination, Navigation]}
+            className={styles.mySwiper}
+            slidesPerView={1}
+            onBeforeInit={(swiper) => {
+              eyesSwiperRef.current = swiper;
+            }}
+          >
+            <SwiperSlide>
+              <img
+                className={styles.eyes1}
+                alt=""
+                src="../eyes_1@2x.png"
+                onClick={() => {
+                  setEyesSort("https://iili.io/HT56oJ4.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.eyes1}
+                alt=""
+                src="../eyes_1@2x.png"
+                onClick={() => {
+                  setEyesSort("https://iili.io/HT56ze2.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.eyes1}
+                alt=""
+                src="../eyes_1@2x.png"
+                onClick={() => {
+                  setEyesSort("https://iili.io/HT56IbS.png");
+                }}
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                className={styles.eyes1}
+                alt=""
+                src="../eyes_1@2x.png"
+                onClick={() => {
+                  setEyesSort("https://iili.io/HT56uz7.png");
+                }}
+              />
+            </SwiperSlide>
+          </Swiper>
+        </div>
+
+        <img
+          className={styles.eyesInner}
+          alt=""
+          src="../polygon-6.svg"
+          onClick={() => eyesSwiperRef.current?.slidePrev()}
+        />
+        <img
+          className={styles.polygonIcon}
+          alt=""
+          src="../polygon-5.svg"
+          onClick={() => eyesSwiperRef.current?.slideNext()}
+        />
       </div>
 
       <div className={styles.skin} id="Skin">
         <div className={styles.skinChild} />
+
+        <div className={styles.skinText}>SKIN</div>
         <img
           className={styles.nametagw7Icon}
           alt=""
           src="../nametagw-7@1x.png"
         />
-        <div className={styles.skin1}>SKIN</div>
-        <img className={styles.skinItem} alt="" src="../ellipse-7.svg" />
-        <img className={styles.skinInner} alt="" src="../ellipse-8.svg" />
-        <img className={styles.ellipseIcon} alt="" src="../ellipse-9.svg" />
+
+        <img
+          className={styles.skin1}
+          alt=""
+          src="../ellipse-7.svg"
+          onClick={() => {
+            setSkinSort("https://iili.io/HT56dbI.png");
+          }}
+        />
+        <img
+          className={styles.skin2}
+          alt=""
+          src="../ellipse-8.svg"
+          onClick={() => {
+            setSkinSort("https://iili.io/HT563xt.png");
+          }}
+        />
+        <img
+          className={styles.skin3}
+          alt=""
+          src="../ellipse-9.svg"
+          onClick={() => {
+            setSkinSort("https://iili.io/HT56FWX.png");
+          }}
+        />
       </div>
+
       <div className={styles.backgroundcolor} id="BackgroundColor">
         <div className={styles.backgroundcolorChild} />
         <img
